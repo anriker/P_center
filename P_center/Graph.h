809ea867b13ce4d4a98267d1ce5d81698b65110f
anriker <1850524578@qq.com>
2018-10-11 08:17:03 +0800

    null

---
+++ P_center/Graph.h
@@ -0,0 +1,20 @@
+#ifndef PCENTER_Graph_H
+#define PCENTER_Graph_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+//class Graph {
+
+//public:
+    struct Nodes {
+        int id;
+        int	x;
+        int y;
+    } ;
+
+    std::vector <Nodes> Node;
+    std::vector<std::vector <int>>distance;
+    int *pcenter;
+//};
+#endif // PCENTER_Graph_H
